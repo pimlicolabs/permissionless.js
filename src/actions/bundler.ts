8c214fcb8e391c89b9cba8e078001ce05f35872a
plusminushalf <garvitdelhi@gmail.com>
2023-09-26 19:07:58 +0100

    Fix supportedEntryPoints type

--- src/actions/bundler.ts
+++ src/actions/bundler.ts
@@ -384,7 +384,7 @@ const bundlerActions = (client: Client) => ({
      *
      * // Return ['0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789']
      */
-    supportedEntryPoints: () => supportedEntryPoints(client as BundlerClient),
+    supportedEntryPoints: (): Promise<Address[]> => supportedEntryPoints(client as BundlerClient),
     /**
      *
      * Returns the supported chain id by the bundler service
