7b1b015d1bd58defb16c33df4744d6ca696eead1
plusminushalf <garvitdelhi@gmail.com>
2023-09-29 11:26:46 +0100

    Add documentation & move biome.json to rome.json as biome support files & rome in vscode is more stable

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { type Address, type Hex, encodeAbiParameters, keccak256 } from "viem"
+import { type Address, type Hash, type Hex, encodeAbiParameters, keccak256 } from "viem"
 import type { UserOperation } from "../types"
 
 function packUserOp({ userOperation }: { userOperation: UserOperation }): Hex {
@@ -34,11 +34,28 @@ function packUserOp({ userOperation }: { userOperation: UserOperation }): Hex {
     )
 }
 
-const getUserOperationHash = ({
-    userOperation,
-    entryPoint,
-    chainId
-}: { userOperation: UserOperation; entryPoint: Address; chainId: bigint }) => {
+export type GetUserOperationHashParams = { userOperation: UserOperation; entryPoint: Address; chainId: bigint }
+
+/**
+ *
+ * Returns user operation hash that is a unique identifier of the user operation.
+ *
+ * @param args: userOperation, entryPoint, chainId as {@link GetUserOperationHashParams}
+ * @returns userOperationHash as {@link Hash}
+ *
+ * @example
+ * import { getUserOperationHash } from "permissionless/utils"
+ *
+ * const userOperationHash = getUserOperationHash({
+ *      userOperation,
+ *      entryPoint,
+ *      chainId
+ * })
+ *
+ * // Returns "0xe9fad2cd67f9ca1d0b7a6513b2a42066784c8df938518da2b51bb8cc9a89ea34"
+ *
+ */
+const getUserOperationHash = ({ userOperation, entryPoint, chainId }: GetUserOperationHashParams): Hash => {
     const encoded = encodeAbiParameters(
         [{ type: "bytes32" }, { type: "address" }, { type: "uint256" }],
         [keccak256(packUserOp({ userOperation })), entryPoint, chainId]
