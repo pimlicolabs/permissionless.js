5782cdb93627779169113072a896bb2aa929f2cb
livingrockrises <90545960+livingrockrises@users.noreply.github.com>
2023-12-21 23:50:15 +0400

    use only optimised selectors

--- src/accounts/biconomy/abi/BiconomySmartAccountAbi.ts
+++ src/accounts/biconomy/abi/BiconomySmartAccountAbi.ts
@@ -28,52 +28,6 @@ export const BiconomyExecuteAbi = [
     {
         inputs: [
             {
-                internalType: "address",
-                name: "dest",
-                type: "address"
-            },
-            {
-                internalType: "uint256",
-                name: "value",
-                type: "uint256"
-            },
-            {
-                internalType: "bytes",
-                name: "func",
-                type: "bytes"
-            }
-        ],
-        name: "execute",
-        outputs: [],
-        stateMutability: "nonpayable",
-        type: "function"
-    },
-    {
-        inputs: [
-            {
-                internalType: "address[]",
-                name: "dest",
-                type: "address[]"
-            },
-            {
-                internalType: "uint256[]",
-                name: "value",
-                type: "uint256[]"
-            },
-            {
-                internalType: "bytes[]",
-                name: "func",
-                type: "bytes[]"
-            }
-        ],
-        name: "executeBatch",
-        outputs: [],
-        stateMutability: "nonpayable",
-        type: "function"
-    },
-    {
-        inputs: [
-            {
                 internalType: "address[]",
                 name: "dest",
                 type: "address[]"
