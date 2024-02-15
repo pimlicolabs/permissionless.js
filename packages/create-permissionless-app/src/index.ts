9f9fc9ecd832372a7c5195ddb9b2aa64f90aaa4a
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2024-02-15 18:25:10 +0530

    chore: include post-setup guidance in src/index.ts
    - Added informative next steps for users after CLI execution.
    - Included some thank you note and apology message for beta issues.
    - Added a Telegram invitation link for users for feedback & potential issues.

--- packages/create-permissionless-app/src/index.ts
+++ packages/create-permissionless-app/src/index.ts
@@ -7,7 +7,10 @@ import { runCli } from "./cli/index"
 import { createProject } from "./helpers/createProject"
 import { installDependencies } from "./helpers/installDependencies"
 import { addPackageDependency } from "./utils/addPackageDependency"
-
+import {
+    type PackageManager,
+    detectPackageManager
+} from "./utils/detectPackageManager"
 import { logger } from "./utils/logger"
 import { parseNameAndPath } from "./utils/parseNameAndPath"
 import { renderTitle } from "./utils/renderTitle"
@@ -25,6 +28,7 @@ const main = async () => {
         appName,
         flags: { noInstall, bundler, paymaster, signer, accountSystem }
     } = await runCli()
+    const packageManager: PackageManager = detectPackageManager()
 
     const [scopedAppName, appDir] = parseNameAndPath(appName)
 
@@ -51,6 +55,41 @@ const main = async () => {
     if (!noInstall) {
         await installDependencies(projectDir)
     }
+
+    // Display next steps for the user
+    logger.info("Next steps:")
+    scopedAppName !== "." && logger.info(`  cd ${scopedAppName}`)
+    if (noInstall) {
+        // To reflect yarn's default behavior of installing packages when no additional args provided
+        if (packageManager === "yarn") {
+            logger.info(`  ${packageManager}`)
+        } else {
+            logger.info(`  ${packageManager} install`)
+        }
+    }
+
+    // Thank you note and apology for beta issues
+    logger.info("\nThank you for using our CLI tool!")
+    logger.info(
+        "As we launch the first version, please pardon any issues during beta testing."
+    )
+    logger.info(
+        "Your feedback helps us improve. If you encounter any bugs or have suggestions, please let us know."
+    )
+    logger.info("We appreciate your support!")
+
+    // Thank you note and apology for beta issues
+    logger.info("\nThank you for using our CLI tool!")
+    logger.info(
+        "If you encounter any issues, please let us know in our Telegram channel. Your feedback helps us improve."
+    )
+    logger.info(
+        "We apologize for any inconvenience during beta testing. Thanks for being patient!"
+    )
+
+    // Telegram invitation link
+    logger.info("\nJoin our Telegram group for support and updates:")
+    logger.info("https://t.me/pimlicoHQ")
 
     process.exit(0)
 }
