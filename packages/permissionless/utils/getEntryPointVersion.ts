d2e796d80c166115d7ce37fb1c665a8775c85e2a
Garvit Khatri <garvitdelhi@gmail.com>
2024-02-22 17:37:33 +0530

    update entryPoint address

--- packages/permissionless/utils/getEntryPointVersion.ts
+++ packages/permissionless/utils/getEntryPointVersion.ts
@@ -7,7 +7,7 @@ import type { EntryPoint, GetEntryPointVersion } from "../types/entrypoint"
 export const ENTRYPOINT_ADDRESS_V06: ENTRYPOINT_ADDRESS_V06_TYPE =
     "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"
 export const ENTRYPOINT_ADDRESS_V07: ENTRYPOINT_ADDRESS_V07_TYPE =
-    "0x7547CAC84A1eF4E6FA8bF83cD89404e7BFB72A4d"
+    "0x0000000071727De22E5E9d8BAf0edAc6f37da032"
 
 export const getEntryPointVersion = (
     entryPoint: EntryPoint
