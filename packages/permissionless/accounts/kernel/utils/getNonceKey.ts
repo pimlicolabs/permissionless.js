979d85ea94d4bbf26a236a3f2d9d827206a02fca
Sahil Vasava <sahilvasava@gmail.com>
2024-04-18 22:58:17 +0530

    feat: add KernelV3 + EPv0.7 account implementation and testcase for Kernel Smart Account

---
+++ packages/permissionless/accounts/kernel/utils/getNonceKey.ts
@@ -0,0 +1,36 @@
+import { concatHex, maxUint16, pad, toHex } from "viem"
+import { VALIDATOR_MODE, VALIDATOR_TYPE } from "../constants"
+import {
+    KERNEL_VERSION_TO_ADDRESSES_MAP,
+    type KernelVersion
+} from "../signerToEcdsaKernelSmartAccount"
+
+export const getNonceKeyWithEncoding = (
+    accountVerion: KernelVersion,
+    nonceKey = 0n
+) => {
+    if (accountVerion === "0.2.2") {
+        return nonceKey
+    }
+
+    if (nonceKey > maxUint16)
+        throw new Error(
+            `nonce key must be equal or less than 2 bytes(maxUint16) for Kernel version ${accountVerion}`
+        )
+
+    const ecdsaValidatorAddress =
+        KERNEL_VERSION_TO_ADDRESSES_MAP["0.3.0-beta"].ECDSA_VALIDATOR // Just use ECDSA as root validator
+    const validatorMode = VALIDATOR_MODE.DEFAULT
+    const validatorType = VALIDATOR_TYPE.ROOT
+    const encoding = pad(
+        concatHex([
+            validatorMode, // 1 byte
+            validatorType, // 1 byte
+            ecdsaValidatorAddress, // 20 bytes
+            toHex(nonceKey, { size: 2 }) // 2 byte
+        ]),
+        { size: 24 }
+    ) // 24 bytes
+    const encodedNonceKey = BigInt(encoding)
+    return encodedNonceKey
+}
