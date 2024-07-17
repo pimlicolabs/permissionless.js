c7585b4d41714126fef5933575b5702c7694f6ba
Garvit Khatri <garvitdelhi@gmail.com>
2024-07-17 13:17:20 +0100

    convert to proper size

--- packages/permissionless/utils/encodeNonce.ts
+++ packages/permissionless/utils/encodeNonce.ts
@@ -1,6 +1,8 @@
-export function encodeNonce({
-    key,
-    sequence
-}: { key: bigint; sequence: bigint }): bigint {
+import { toHex } from "viem"
+
+export function encodeNonce(args: { key: bigint; sequence: bigint }): bigint {
+    const key = BigInt(toHex(args.key, { size: 24 }))
+    const sequence = BigInt(toHex(args.sequence, { size: 8 }))
+
     return (key << BigInt(64)) + sequence
 }
