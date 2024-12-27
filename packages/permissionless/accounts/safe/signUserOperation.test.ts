fe98c6a71b35e880cc1d1fcb7a6b3e9195ef64f7
Garvit Khatri <garvitdelhi@gmail.com>
2024-12-27 15:47:10 +0000

    Fix signUserOperation.test.ts

--- packages/permissionless/accounts/safe/signUserOperation.test.ts
+++ packages/permissionless/accounts/safe/signUserOperation.test.ts
@@ -185,94 +185,6 @@ describe("signUserOperation", () => {
         expect(receipt.success).toBeTruthy()
     })
 
-    testWithRpc("signUserOperation_V06 7579", async ({ rpc }) => {
-        const owners = [
-            privateKeyToAccount(generatePrivateKey()),
-            privateKeyToAccount(generatePrivateKey()),
-            privateKeyToAccount(generatePrivateKey())
-        ]
-
-        const safeAccountClient = getBundlerClient({
-            account: await getSafeClient({
-                ...rpc,
-                entryPoint: {
-                    version: "0.6"
-                },
-                owners: owners.map((owner) => toAccount(owner.address)),
-                erc7579: true
-            }),
-            entryPoint: {
-                version: "0.6"
-            },
-            ...rpc
-        })
-
-        const unSignedUserOperation =
-            await safeAccountClient.prepareUserOperation({
-                calls: [
-                    {
-                        to: safeAccountClient.account.address,
-                        data: "0x"
-                    }
-                ]
-            })
-
-        let partialSignatures = await signUserOperation({
-            version: "1.4.1",
-            entryPoint: {
-                address: entryPoint06Address,
-                version: "0.6"
-            },
-            chainId: foundry.id,
-            owners: owners.map((owner) => toAccount(owner.address)),
-            account: owners[0],
-            safe4337ModuleAddress: "0x7579EE8307284F293B1927136486880611F20002",
-            ...unSignedUserOperation
-        })
-
-        partialSignatures = await signUserOperation({
-            version: "1.4.1",
-            entryPoint: {
-                address: entryPoint06Address,
-                version: "0.6"
-            },
-            chainId: foundry.id,
-            owners: owners.map((owner) => toAccount(owner.address)),
-            account: owners[1],
-            signatures: partialSignatures,
-            safe4337ModuleAddress: "0x7579EE8307284F293B1927136486880611F20002",
-            ...unSignedUserOperation
-        })
-
-        const finalSignature = await signUserOperation({
-            version: "1.4.1",
-            entryPoint: {
-                address: entryPoint06Address,
-                version: "0.6"
-            },
-            chainId: foundry.id,
-            owners: owners.map((owner) => toAccount(owner.address)),
-            account: owners[2],
-            safe4337ModuleAddress: "0x7579EE8307284F293B1927136486880611F20002",
-            signatures: partialSignatures,
-            ...unSignedUserOperation
-        })
-
-        const userOpHash = safeAccountClient.sendUserOperation({
-            ...unSignedUserOperation,
-            signature: finalSignature
-        })
-
-        expect(userOpHash).toBeTruthy()
-
-        const receipt = await safeAccountClient.waitForUserOperationReceipt({
-            hash: await userOpHash
-        })
-
-        expect(receipt).toBeTruthy()
-        expect(receipt.success).toBeTruthy()
-    })
-
     testWithRpc("signUserOperation_V07 7579", async ({ rpc }) => {
         const { anvilRpc } = rpc
 
@@ -316,6 +228,7 @@ describe("signUserOperation", () => {
             chainId: foundry.id,
             owners: owners.map((owner) => toAccount(owner.address)),
             account: owners[0],
+            safe4337ModuleAddress: "0x7579EE8307284F293B1927136486880611F20002",
             ...unSignedUserOperation
         })
 
@@ -329,6 +242,7 @@ describe("signUserOperation", () => {
             owners: owners.map((owner) => toAccount(owner.address)),
             account: owners[1],
             signatures: partialSignatures,
+            safe4337ModuleAddress: "0x7579EE8307284F293B1927136486880611F20002",
             ...unSignedUserOperation
         })
 
@@ -342,6 +256,7 @@ describe("signUserOperation", () => {
             owners: owners.map((owner) => toAccount(owner.address)),
             account: owners[2],
             signatures: partialSignatures,
+            safe4337ModuleAddress: "0x7579EE8307284F293B1927136486880611F20002",
             ...unSignedUserOperation
         })
 
