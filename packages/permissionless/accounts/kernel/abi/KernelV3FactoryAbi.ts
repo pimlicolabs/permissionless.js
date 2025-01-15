6ea966da1c0851168af1e924681d75d4a3e6f3e3
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2025-01-15 14:17:42 +0000

    remove unused functions

--- packages/permissionless/accounts/kernel/abi/KernelV3FactoryAbi.ts
+++ packages/permissionless/accounts/kernel/abi/KernelV3FactoryAbi.ts
@@ -1,10 +1,5 @@
 export const KernelV3FactoryAbi = [
     {
-        type: "constructor",
-        inputs: [{ name: "_impl", type: "address", internalType: "address" }],
-        stateMutability: "nonpayable"
-    },
-    {
         type: "function",
         name: "createAccount",
         inputs: [
@@ -13,23 +8,5 @@ export const KernelV3FactoryAbi = [
         ],
         outputs: [{ name: "", type: "address", internalType: "address" }],
         stateMutability: "payable"
-    },
-    {
-        type: "function",
-        name: "getAddress",
-        inputs: [
-            { name: "data", type: "bytes", internalType: "bytes" },
-            { name: "salt", type: "bytes32", internalType: "bytes32" }
-        ],
-        outputs: [{ name: "", type: "address", internalType: "address" }],
-        stateMutability: "view"
-    },
-    {
-        type: "function",
-        name: "implementation",
-        inputs: [],
-        outputs: [{ name: "", type: "address", internalType: "address" }],
-        stateMutability: "view"
-    },
-    { type: "error", name: "InitializeError", inputs: [] }
+    }
 ] as const
